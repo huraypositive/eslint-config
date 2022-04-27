771a205a25e63d590bce4882e97cfa88a7a74aa1
yyounghoon <yhyoon@huray.net>
2022-04-27 18:17:50 +0900

    version 1.0.3

--- react.js
+++ react.js
@@ -11,6 +11,8 @@ module.exports = {
         },
     },
     rules: {
+        'react/prop-types': 'off', // react prop-types 필수로 적용안해도 됨
+        'no-console': 'off', // 콘솔 사용 허용,
         'react/jsx-filename-extension': ['error', {
             extensions: ['.jsx', '.tsx'],
         }],
