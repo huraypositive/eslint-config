c74ec90a5c8111cfb1f217717e606da8f6223f5f
yyounghoon <yhyoon@huray.net>
2022-11-15 18:59:05 +0900

    1.0.7

--- react.js
+++ react.js
@@ -13,8 +13,40 @@ module.exports = {
     rules: {
         'react/prop-types': 'off', // react prop-types 필수로 적용안해도 됨
         'react/jsx-filename-extension': ['error', { // js,tsx파일에서 jsx문법을 사용가능하게 확장
-            extensions: ['.js', '.tsx'],
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
         }],
-        'import/no-unresolved': 'off', // 절대경로 사용하기 위해 설정
+        'import/no-unresolved': 'off', // 절대경로 사용하기 위해 설정,
+        'react/react-in-jsx-scope': 'off',
+        'import/prefer-default-export': 'off',
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+                json: 'never',
+            },
+        ],
+        '@emotion/jsx-import': 'error',
+        'react/jsx-props-no-spreading': 'off',
+        'react/require-default-props': 'off',
+        'no-param-reassign': [
+            'error',
+            {
+                props: true,
+                ignorePropertyModificationsFor: ['state'],
+            },
+        ],
+        'import/no-extraneous-dependencies': ['error', {devDependencies: true}],
+        'react/no-array-index-key': 'off',
+        'react-hooks/exhaustive-deps': [
+            'warn',
+            {
+                additionalHooks: 'useRecoilCallback',
+            },
+        ],
+        'consistent-return': 'off',
     },
 };
